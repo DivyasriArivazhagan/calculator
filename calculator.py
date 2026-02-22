12058f73aa98d0e8b6e9343567531292a1adff8c
Divyasriarivazhagan <divyasriselvi28@gmail.com>
2026-02-22 14:20:07 +0530

    first commit

---
+++ calculator.py
@@ -0,0 +1,27 @@
+# simple_calculator.py
+
+print("Simple Calculator")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Select operation: +  -  *  /")
+choice = input("Enter operation: ")
+
+if choice == "+":
+    print("Result =", num1 + num2)
+
+elif choice == "-":
+    print("Result =", num1 - num2)
+
+elif choice == "*":
+    print("Result =", num1 * num2)
+
+elif choice == "/":
+    if num2 != 0:
+        print("Result =", num1 / num2)
+    else:
+        print("Error! Cannot divide by zero.")
+
+else:
+    print("Invalid operation.")
